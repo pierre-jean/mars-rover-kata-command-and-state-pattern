40954f394d14b5bcfc0cdf424df82c0729a9691e
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2018-07-13 14:08:32 +0100

    Parameterized tests

--- src/test/java/com/codurance/katas/MarsRoverShould.java
+++ src/test/java/com/codurance/katas/MarsRoverShould.java
@@ -1,13 +1,19 @@
 package com.codurance.katas;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.io.ByteArrayInputStream;
 import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@RunWith(JUnitParamsRunner.class)
 public class MarsRoverShould {
 
     public static final String INPUT = String.join("\r",
@@ -18,8 +24,9 @@ public class MarsRoverShould {
             "MMRMMRMRRM");
 
     @Test
-    public void return_the_right_output() {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(INPUT.getBytes());
+    @Parameters(method = "getScenarios")
+    public void return_the_right_output(String input, List<String> output) {
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(input.getBytes());
         PrintStream outputStream = mock(PrintStream.class);
         System.setIn(inputStream);
         System.setOut(outputStream);
@@ -27,7 +34,24 @@ public class MarsRoverShould {
         MarsRoverApp app = new MarsRoverApp();
         app.execute();
 
-        verify(outputStream).println("1 3 N");
-        verify(outputStream).println("5 1 E");
+        output.stream().forEach( line -> verify( outputStream).println(line));
+    }
+
+    Object[][] getScenarios(){
+       return new Object[][]{
+               new Object[]{
+                       createInputFor("5 5", "1 2 N", "LMLMLMLMM", "3 3 E", "MMRMMRMRRM"),
+                       createOutputFor("1 3 N", "5 1 E")
+               }
+       };
+    }
+
+
+    private Object createOutputFor(String... results){
+        return Arrays.asList(results);
+    }
+
+    private Object createInputFor(String... commands){
+        return String.join("\r", commands);
     }
 }
